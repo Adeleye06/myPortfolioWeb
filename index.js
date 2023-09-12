16d7228310e843119053eddbbea8fd8507b28b2a
Enoch Adeleye <enochadeleye3@gmail.com>
2023-09-12 17:01:23 -0600

    added some things ande made my form work as needed.

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
 
-
+app.get('/', (req, res) => {
+    res.redirect('about');
+})
 app.get('/about', (req, res) => {
     res.render('about');
 })
@@ -18,9 +20,19 @@ app.get('/projects', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact');
 })
-app.get('/', (req, res) => {
-    res.redirect('about');
+
+app.get('/files/:fileName', (req, res) => {
+    const fileName = req.params.fileName;
+    const filePath = path.join(__dirname, 'files', fileName);
+
+   try {
+     res.sendFile(filePath);
+   } catch (error) {
+        console.log('error sending file:', error);
+        res.status(500).send('Internal Server Error');
+   }
 })
+
 app.listen(3000, () => {
     console.log('Serving at port 3000');
 })
